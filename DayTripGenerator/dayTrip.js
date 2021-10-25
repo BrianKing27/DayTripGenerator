79505652f0e9131e9c9e3e3d0e1c5e081f394569
BrianKing27 <93146464+BrianKing27@users.noreply.github.com>
2021-10-25 13:34:41 -0400

    Add files via upload

---
+++ DayTripGenerator/dayTrip.js
@@ -0,0 +1,29 @@
+'use strict'
+let destinations = ['D.C.', 'Baltimore', 'New York City', 'Boston', 'Los Angeles', 'San Fransisco', 'Baja', 'Houston', 'Austin', 'San Antonio'];
+let restuarants = ["Carrabba's Italian Grill", "Olive Garden", "Red Lobster", "Chili's", "Apple Bee's", "P.F. Chang's China Bistro"]
+let transportation = ["Car rental", "Taxi", "Moped", "Bike", "Walking"]
+let entertainment =["Zoo", "Museum", "Aquarium"]
+
+function destination(){  // A random Destination
+    let ranDest = Math.floor(Math.random() * 9) + 1;
+    return destinations[ranDest];
+};
+
+function restuarant(){  // A random restuarant
+    let ranRest = Math.floor(Math.random() * 5) + 1;
+    return Restuarant[ranRest];
+};
+
+function transportation(){  // Random Transportation methods.
+    let ranTran = Math.floor(Math.random() * 5) + 1;
+    return transportation[ranTran];
+};
+ function entertainment(){  // Random Entertainment idea.
+    let ranEnt = Math.floor(Math.random() * 10) + 1;
+    return Entertainment[ranEnt];
+};
+
+
+
+
+console.log(destination());
