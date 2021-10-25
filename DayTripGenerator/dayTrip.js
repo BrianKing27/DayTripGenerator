aa972649b1694554ecc0db1baf0e2e71cafcfef9
unknown <brianking2992@gmail.com>
2021-10-25 13:50:49 -0400

    Wrote alert function to test random generator for destination, restaurant, transportation and entertainment functions.

--- DayTripGenerator/dayTrip.js
+++ DayTripGenerator/dayTrip.js
@@ -2,7 +2,7 @@
 let destinations = ['D.C.', 'Baltimore', 'New York City', 'Boston', 'Los Angeles', 'San Fransisco', 'Baja', 'Houston', 'Austin', 'San Antonio'];
 let restuarants = ["Carrabba's Italian Grill", "Olive Garden", "Red Lobster", "Chili's", "Apple Bee's", "P.F. Chang's China Bistro"]
 let transportation = ["Car rental", "Taxi", "Moped", "Bike", "Walking"]
-let entertainment =["Zoo", "Museum", "Aquarium"]
+let entertainment =["Zoo", "Museum", "Aquarium", "Hiking", "Ski/Snowboard", "Concert"]
 
 function destination(){  // A random Destination
     let ranDest = Math.floor(Math.random() * 9) + 1;
@@ -15,15 +15,15 @@ function restuarant(){  // A random restuarant
 };
 
 function transportation(){  // Random Transportation methods.
-    let ranTran = Math.floor(Math.random() * 5) + 1;
+    let ranTran = Math.floor(Math.random() * 4) + 1;
     return transportation[ranTran];
 };
  function entertainment(){  // Random Entertainment idea.
-    let ranEnt = Math.floor(Math.random() * 10) + 1;
+    let ranEnt = Math.floor(Math.random() * 5) + 1;
     return Entertainment[ranEnt];
 };
 
 
 
 
-console.log(destination());
+alert(destination(), restuarant(), transportation(), entertainment());
