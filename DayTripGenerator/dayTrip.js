39e174c7b5894775cf0776595e12dabf99a9d22d
unknown <briankingDCC@gmail.com>
2021-10-25 14:34:37 -0400

    Created itinerary variable and called with alert function to display randomly generated itinerary

--- DayTripGenerator/dayTrip.js
+++ DayTripGenerator/dayTrip.js
@@ -1,29 +1,38 @@
 'use strict'
+
 let destinations = ['D.C.', 'Baltimore', 'New York City', 'Boston', 'Los Angeles', 'San Fransisco', 'Baja', 'Houston', 'Austin', 'San Antonio'];
-let restuarants = ["Carrabba's Italian Grill", "Olive Garden", "Red Lobster", "Chili's", "Apple Bee's", "P.F. Chang's China Bistro"]
-let transportation = ["Car rental", "Taxi", "Moped", "Bike", "Walking"]
-let entertainment =["Zoo", "Museum", "Aquarium", "Hiking", "Ski/Snowboard", "Concert"]
+let restaurants = ["Carrabba's Italian Grill", "Olive Garden", "Red Lobster", "Chili's", "Apple Bee's", "P.F. Chang's China Bistro"]
+let transportations = ["Car rental", "Taxi", "Moped", "Bike", "Walking"]
+let entertainments =["Zoo", "Museum", "Aquarium", "Hiking", "Ski/Snowboard", "Concert"]
 
 function destination(){  // A random Destination
     let ranDest = Math.floor(Math.random() * 9) + 1;
     return destinations[ranDest];
 };
 
-function restuarant(){  // A random restuarant
+function restaurant(){  // A random restuarant
     let ranRest = Math.floor(Math.random() * 5) + 1;
-    return Restuarant[ranRest];
+    return restaurants[ranRest];
 };
 
 function transportation(){  // Random Transportation methods.
     let ranTran = Math.floor(Math.random() * 4) + 1;
-    return transportation[ranTran];
+    return transportations[ranTran];
 };
  function entertainment(){  // Random Entertainment idea.
     let ranEnt = Math.floor(Math.random() * 5) + 1;
-    return Entertainment[ranEnt];
+    return entertainments[ranEnt];
 };
 
 
 
 
-alert(destination(), restuarant(), transportation(), entertainment());
+
+let itinerary = `
+Destination: ${destination()}
+Restaurant: ${restaurant()}
+Transportation: ${transportation()}
+Entertainment: ${entertainment()}`
+
+
+alert(itinerary);
